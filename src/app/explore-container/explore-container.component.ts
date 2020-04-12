27fb9379a88ae8bacebd7b4ca5e98185f0463064
abastola <abastol1@ramapo.edu>
2020-04-12 02:15:27 -0400

    Chat Flow

--- src/app/explore-container/explore-container.component.ts
+++ src/app/explore-container/explore-container.component.ts
@@ -17,61 +17,237 @@ export class ExploreContainerComponent implements OnInit {
     public modalController: ModalController
   ) { }
 
-  userage: string;
+  countYour = 0;
+  countMine = 0;
+  elementYour = document.getElementsByClassName('yours') as HTMLCollectionOf<HTMLElement>;
+  elementMine = document.getElementsByClassName('mine') as HTMLCollectionOf<HTMLElement>;
+  symptomsArray: Set<string> = new Set<string>();
+  additionalSympArray: Set<string> = new Set<string>();
+  historyDisease: Set<string> = new Set<string>();
+  userage: number;
   ngOnInit() {
     this.presentLoading();
-    // document.getElementById('langAns').style.display = 'none';
-    // document.getElementById('ageQues').style.display = 'none';
-    // document.getElementById('ageInput').style.display = 'none';
-    // document.getElementById('ageAns').style.display = 'none';
-    // document.getElementById('genderQues').style.display = 'none';
-    // document.getElementById('genderAns').style.display = 'none';
-    // document.getElementById('temperatureQues').style.display = 'none';
-    // document.getElementById('symptoms').style.display = 'none';
-    // document.getElementById('symptomAns').style.display = 'none';
-    // document.getElementById('temperatureAnswer').style.display = 'none';
-    // document.getElementById('modalButton').style.display = 'none'
+
+  }
+
+  ionViewWillEnter() {
+    // tslint:disable-next-line:prefer-for-of
+    console.log("ENTER");
+    alert("ENTER");
+    for (let i = 0; i < this.elementYour.length; i++) {
+      this.elementYour[i].style.display = 'none';
+    }
+    this.ShowElementYour();
+    // tslint:disable-next-line:prefer-for-of
+    for (let i = 0; i < this.elementMine.length; i++) {
+      this.elementMine[i].style.display = 'none';
+    }
+    document.getElementById('result').style.display = 'none';
+
   }
 
   async presentLoading() {
     const loading = await this.loadingController.create({
       message: 'Please wait...',
-      duration: 500
+      duration: 200
     });
     return await loading.present();
   }
 
-  languageClicked(languageName) {
-    this.presentLoading();
-    this.presentLoading().then();
-    document.getElementById('langAns').style.display = 'block';
-    document.getElementById('ageQues').style.display = 'block';
-    document.getElementById('ageInput').style.display = 'block';
+  ShowElementMine() {
+    this.elementMine[this.countMine].style.display = 'flex';
+    this.countMine++;
+  }
+  ShowElementYour() {
+    this.elementYour[this.countYour].style.display = 'flex';
+    this.countYour++;
+  }
 
+  languageClicked(event, languageName) {
+    this.presentLoading();
+    // tslint:disable-next-line:no-string-literal
+    event['target']['parentNode'].style.display = 'none';
+    let el = this.elementMine[this.countMine].childNodes[0] as HTMLElement;
+    el.innerHTML = languageName;
+    // this.elementMine[this.countMine].childNodes[0].innerHTML = languageName;
+    this.ShowElementMine();
+    this.ShowElementYour();
+    this.ShowElementMine();
   }
 
   AgeInput() {
-    document.getElementById('ageInput').style.display = 'none';
-    document.getElementById('ageAnsText').innerHTML = this.userage;
-    document.getElementById('ageAns').style.display = 'block';
+    if (this.userage === 0 || this.userage === undefined) {
+      return;
+    }
+    this.elementMine[this.countMine - 1].style.display = 'none';
+    let el = this.elementMine[this.countMine].childNodes[0] as HTMLElement;
+    el.innerHTML = 'Age: ' + this.userage;
+    // this.elementMine[this.countMine].childNodes[0].innerHTML = 'Age: ' + this.userage;
+    this.ShowElementMine();
+    this.ShowElementYour();
+  }
 
-    document.getElementById('genderQues').style.display = 'block';
+  GenderClicked(event, gender) {
+    event['target']['parentNode'].style.display = 'none';
+    let el = this.elementMine[this.countMine].childNodes[0] as HTMLElement;
+    el.innerHTML = gender
+    // this.elementMine[this.countMine].childNodes[0].innerHTML = gender;
+    this.ShowElementMine();
+    this.ShowElementYour();
+  }
+
+  TempClicked(event, temperature) {
+    event['target']['parentNode'].style.display = 'none';
+    let el = this.elementMine[this.countMine].childNodes[0] as HTMLElement;
+    el.innerHTML = temperature;
+    // this.elementMine[this.countMine].childNodes[0].innerHTML = temperature;
+    this.ShowElementMine();
+    this.ShowElementYour();
+  }
+
+  SymptomClicked(event, sym) {
+    if (this.symptomsArray.has(sym)) {
+      this.symptomsArray.delete(sym);
+      event['toElement']['color'] = 'light';
+    }
+    else {
+      this.symptomsArray.add(sym);
+      event['toElement']['color'] = 'danger';
+    }
+
+    if (this.symptomsArray.size === 0) {
+      document.getElementById('confirmSymptom').innerHTML = 'None of these';
+    }
+    else {
+      document.getElementById('confirmSymptom').innerHTML = 'Confirm';
+      let att = document.createAttribute("color");
+      att.value = 'success';
+      document.getElementById('confirmSymptom').setAttributeNode(att);
+    }
 
   }
 
-  GenderClicked(gender) {
-    document.getElementById('gender').innerHTML = gender;
-    document.getElementById('genderAns').style.display = 'block';
-    document.getElementById('temperatureQues').style.display = 'block';
+  SymptomConfirmed(event) {
+    event['target']['parentNode'].style.display = 'none';
+
+    let status = document.getElementById('confirmSymptom').innerHTML;
+    console.log("Symptoms: ", this.symptomsArray);
+    if (status == 'Confirm') {
+      status = '';
+      let array = Array.from(this.symptomsArray);
+      status = array.join(', ');
+      // for (let item of this.symptomsArray) {
+      //   status = item + ',' + status;
+      // }
+    }
+    let el = this.elementMine[this.countMine].childNodes[0] as HTMLElement;
+    el.innerHTML = status;
+    // this.elementMine[this.countMine].childNodes[0].innerHTML = status;
+    this.ShowElementMine();
+    this.ShowElementYour();
+
   }
 
-  TempClicked(temperature) {
-    // alert(temperature);
-    document.getElementById('temperature').innerHTML = temperature;
-    document.getElementById('temperatureAnswer').style.display = 'block';
-    // document.getElementById('symptomsText').innerHTML = 'Cough, Shortness of breath';
-    document.getElementById('symptoms').style.display = 'block';
-    // document.getElementById('symptomAns').style.display = 'block';
+  AdditionalSymp(event, sym) {
+    if (this.additionalSympArray.has(sym)) {
+      this.additionalSympArray.delete(sym);
+      event['toElement']['color'] = 'light';
+    }
+    else {
+      this.additionalSympArray.add(sym);
+      event['toElement']['color'] = 'danger';
+    }
+
+    if (this.additionalSympArray.size === 0) {
+      document.getElementById('ConfirmAddSymp').innerHTML = 'None of these';
+    }
+    else {
+      document.getElementById('ConfirmAddSymp').innerHTML = 'Confirm';
+      let att = document.createAttribute("color");
+      att.value = 'success';
+      document.getElementById('ConfirmAddSymp').setAttributeNode(att);
+    }
+
+  }
+
+  ConfirmAdditionalSymp(event) {
+    event['target']['parentNode'].style.display = 'none';
+
+    let status = document.getElementById('ConfirmAddSymp').innerHTML;
+    console.log("Symptoms: ", this.additionalSympArray);
+    if (status == 'Confirm') {
+      status = '';
+      let array = Array.from(this.additionalSympArray);
+      status = array.join(', ');
+      // for (let item of this.additionalSympArray) {
+      //   status = item + ',' + status;
+      // }
+    }
+    let el = this.elementMine[this.countMine].childNodes[0] as HTMLElement;
+    el.innerHTML = status;
+    // this.elementMine[this.countMine].childNodes[0].innerHTML = status;
+    this.ShowElementMine();
+    this.ShowElementYour();
+
+  }
+
+  History(event, commute) {
+    event['target']['parentNode'].style.display = 'none';
+    let el = this.elementMine[this.countMine].childNodes[0] as HTMLElement;
+    el.innerHTML = commute;
+    // this.elementMine[this.countMine].childNodes[0].innerHTML = commute;
+    this.ShowElementMine();
+    this.ShowElementYour();
+  }
+
+  DiseaseClicked(event, disease) {
+    if (this.historyDisease.has(disease)) {
+      this.historyDisease.delete(disease);
+      event['toElement']['color'] = 'light';
+
+    }
+    else {
+      this.historyDisease.add(disease);
+      event['toElement']['color'] = 'danger';
+
+    }
+
+    if (this.historyDisease.size === 0) {
+      document.getElementById('confirmHistory').innerHTML = 'None of these';
+    }
+    else {
+      document.getElementById('confirmHistory').innerHTML = 'Confirm';
+      let att = document.createAttribute("color");
+      att.value = 'success';
+      document.getElementById('confirmHistory').setAttributeNode(att);
+    }
+  }
+
+  ConfirmHistoryDisease(event) {
+    event['target']['parentNode'].style.display = 'none';
+    let status = document.getElementById('ConfirmAddSymp').innerHTML;
+    console.log("Symptoms: ", this.historyDisease);
+    if (status == 'Confirm') {
+      status = '';
+      let array = Array.from(this.historyDisease);
+      status = array.join(', ');
+    }
+    let el = this.elementMine[this.countMine].childNodes[0] as HTMLElement;
+    el.innerHTML = status;
+    // this.elementMine[this.countMine].childNodes[0].innerHTML = status;
+    this.ShowElementMine();
+    this.ShowElementYour();
+
+  }
+
+  Progress(event, prog) {
+    event['target']['parentNode'].style.display = 'none';
+    let el = this.elementMine[this.countMine].childNodes[0] as HTMLElement;
+    el.innerHTML = prog;
+    // this.elementMine[this.countMine].childNodes[0].innerHTML = prog;
+    this.ShowElementMine();
+
+    document.getElementById('result').style.display = 'block';
   }
 
   ShowModal() {
